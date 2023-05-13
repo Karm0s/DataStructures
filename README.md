8bb786e6d91d96f958963db7097b7bb1dfad86cb
Karm0s <bouchenna.yanis@protonmail.com>
2023-05-13 12:59:50 -0400

    Create default constructor and [] operator overload.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DataStructures
+
+Custom implementation of commonly used data structures in C++.
--- src/includes/DS/DynamicArray.h
+++ src/includes/DS/DynamicArray.h
@@ -1,3 +1,30 @@
 #pragma once
 
-// Dynamic Array
+#include <iostream>
+
+template <typename T>
+class DynamicArray
+{
+private:
+	std::size_t m_Size;
+	T* m_Array;
+
+public:
+	DynamicArray(const std::size_t size = 10)
+	{
+		m_Size = size;
+		m_Array = new T[m_Size];
+	}
+
+	T& operator[](int index)
+	{
+		if (index >= m_Size)
+			throw std::out_of_range("Index out of bound!");
+		return m_Array[index];
+	}
+
+	~DynamicArray()
+	{
+		delete[] m_Array;
+	}
+};
