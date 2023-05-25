060dc7f0cbdd3e07f0ed66d78b2ca3e19992e451
Karm0s <bouchenna.yanis@protonmail.com>
2023-05-24 22:02:05 -0400

    Reformat files.

--- src/includes/DS/dynamic_array.h
+++ src/includes/DS/dynamic_array.h
@@ -8,35 +8,36 @@
 #define MIN_SIZE 2
 #define SHRINK_RATIO 2.5
 
-namespace DS{
+namespace DS 
+{
 
 	template <typename T>
 	class DynamicArray
 	{
 	public:
-		using value_type		=	T;
-		using size_type			=	std::size_t;
-		using pointer			=	DynamicArray::value_type*;
-		using reference			=	DynamicArray::value_type&;
-		using iterator			=	DS::RandomAccessIterator<T>;
-		using const_pointer		=	const pointer;
-		using const_reference	=	const reference;
-		using const_iterator	=	const iterator;
+		using value_type = T;
+		using size_type = std::size_t;
+		using pointer = DynamicArray::value_type*;
+		using reference = DynamicArray::value_type&;
+		using iterator = DS::RandomAccessIterator<T>;
+		using const_pointer = const pointer;
+		using const_reference = const reference;
+		using const_iterator = const iterator;
 
 
-		DynamicArray(): 
-			m_Size(MIN_SIZE), 
-			m_UsedCapacity(0), 
+		DynamicArray()
+			:m_Size(MIN_SIZE),
+			m_UsedCapacity(0),
 			m_Array(new value_type[m_Size]) {}
 
 		DynamicArray(const size_type size)
-			:m_Size(size ? size : MIN_SIZE), 
-			m_UsedCapacity(0), 
+			:m_Size(size ? size : MIN_SIZE),
+			m_UsedCapacity(0),
 			m_Array(new value_type[m_Size]) {}
 
 		DynamicArray(const DynamicArray& other)
-			:m_Size(other.m_Size), 
-			m_UsedCapacity(other.m_UsedCapacity), 
+			:m_Size(other.m_Size),
+			m_UsedCapacity(other.m_UsedCapacity),
 			m_Array(new value_type[m_Size])
 		{
 			std::copy(other.m_Array, other.m_Array + m_Size, this->m_Array);
@@ -51,9 +52,9 @@ namespace DS{
 		}
 
 		DynamicArray(std::initializer_list<value_type> elements)
-		:m_Size(elements.size() ? elements.size() : MIN_SIZE),
-		m_UsedCapacity(m_Size),
-		m_Array(new value_type[m_Size])
+			:m_Size(elements.size() ? elements.size() : MIN_SIZE),
+			m_UsedCapacity(m_Size),
+			m_Array(new value_type[m_Size])
 		{
 			std::copy(elements.begin(), elements.end(), m_Array);
 		}
@@ -70,14 +71,45 @@ namespace DS{
 		}
 
 
-		reference front()					{return m_Array[0];}
-		const_reference front() const		{return m_Array[0];}
-		reference back()					{return m_Array[m_UsedCapacity - 1];}
-		const_reference back() const		{return m_Array[m_UsedCapacity - 1];}
-		iterator begin()					{return iterator(m_Array);}
-		const_iterator begin() const		{return iterator(m_Array);}
-		iterator end()						{return iterator(m_Array + m_UsedCapacity);}
-		const_iterator end() const			{return iterator(m_Array + m_UsedCapacity);}
+		reference front() 
+		{ 
+			return m_Array[0]; 
+		}
+
+		const_reference front() const 
+		{ 
+			return m_Array[0]; 
+		}
+
+		reference back() 
+		{ 
+			return m_Array[m_UsedCapacity - 1]; 
+		}
+
+		const_reference back() const 
+		{ 
+			return m_Array[m_UsedCapacity - 1]; 
+		}
+
+		iterator begin() 
+		{ 
+			return iterator(m_Array); 
+		}
+
+		const_iterator begin() const 
+		{ 
+			return iterator(m_Array); 
+		}
+
+		iterator end() 
+		{ 
+			return iterator(m_Array + m_UsedCapacity); 
+		}
+
+		const_iterator end() const 
+		{ 
+			return iterator(m_Array + m_UsedCapacity); 
+		}
 
 
 		void push_back(const_reference element)
@@ -130,11 +162,25 @@ namespace DS{
 				reallocate(m_Size / 2);
 		}
 
-		reference operator[](size_type index)					{return m_Array[index];}
-		const_reference operator[](size_type index) const		{return m_Array[index];}
+		reference operator[](size_type index) 
+		{ 
+			return m_Array[index]; 
+		}
 
-		reference at(size_type index)							{validateIndex(index); return m_Array[index];}
-		const_reference at(size_type index) const				{validateIndex(index); return m_Array[index];}
+		const_reference operator[](size_type index) const 
+		{
+			return m_Array[index]; 
+		}
+
+		reference at(size_type index) 
+		{
+			validateIndex(index); return m_Array[index];
+		}
+
+		const_reference at(size_type index) const
+		{
+			validateIndex(index); return m_Array[index];
+		}
 
 		friend void swap(DynamicArray& first, DynamicArray& second)
 		{
@@ -149,11 +195,11 @@ namespace DS{
 		size_type m_UsedCapacity;
 
 		pointer m_Array;
-		
+
 		void reallocate(size_type newSize)
 		{
 			value_type* tempArray = new value_type[newSize];
-			
+
 			std::copy(m_Array, m_Array + std::min(newSize, m_Size), tempArray);
 
 			std::swap(tempArray, m_Array);
--- src/includes/DS/iterators.h
+++ src/includes/DS/iterators.h
@@ -6,29 +6,61 @@ namespace DS
 {
 
 	template <typename T>
-	class RandomAccessIterator 
+	class RandomAccessIterator
 	{
 	public:
-		using interator_category	=	std::random_access_iterator_tag;
-		using value_type			=	T;
-		using pointer				=	RandomAccessIterator::value_type*;
-		using reference				=	RandomAccessIterator::value_type&;
-		using const_pointer			=	const pointer;
-		using const_reference		=	const reference;
-		using difference_type		=	std::ptrdiff_t;
+		using interator_category = std::random_access_iterator_tag;
+		using value_type = T;
+		using pointer = RandomAccessIterator::value_type*;
+		using reference = RandomAccessIterator::value_type&;
+		using const_pointer = const pointer;
+		using const_reference = const reference;
+		using difference_type = std::ptrdiff_t;
 
 		RandomAccessIterator(pointer ptr) :m_Pointer(ptr) {}
 
-		reference operator*() { return *m_Pointer; }
-		pointer operator->() { return m_Pointer; }
-		const_reference operator*() const { return *m_Pointer; }
-		const_pointer operator->() const { return m_Pointer; }
+		reference operator*()
+		{
+			return *m_Pointer;
+		}
 
-		RandomAccessIterator& operator++() { m_Pointer++; return *this; }
-		RandomAccessIterator operator++(int) { RandomAccessIterator copy = *this; ++(*this); return copy; }
+		pointer operator->() 
+		{
+			return m_Pointer;
+		}
 
-		bool operator==(const RandomAccessIterator& other) const { return (m_Pointer == other.m_Pointer); }
-		bool operator!=(const RandomAccessIterator& other) const { return (m_Pointer != other.m_Pointer); }
+		const_reference operator*() const
+		{
+			return *m_Pointer;
+		}
+
+		const_pointer operator->() const
+		{
+			return m_Pointer;
+		}
+
+		RandomAccessIterator& operator++()
+		{
+			m_Pointer++;
+			return *this;
+		}
+
+		RandomAccessIterator operator++(int)
+		{
+			RandomAccessIterator copy = *this;
+			++(*this);
+			return copy; 
+		}
+
+		bool operator==(const RandomAccessIterator& other) const
+		{
+			return (m_Pointer == other.m_Pointer);
+		}
+
+		bool operator!=(const RandomAccessIterator& other) const
+		{
+			return (m_Pointer != other.m_Pointer);
+		}
 
 	private:
 		pointer m_Pointer;
